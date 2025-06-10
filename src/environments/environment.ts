c09c05438cfc3a4a5746f73b5939310463499554
Aditya Gupta <aadigupta2946@gmail.com>
2025-06-10 20:55:41 +0530

    Update project name to Codezy, configure environments, and update Firebase hosting path

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8081',
-  wsTerminalUrl: 'ws://localhost:8081/terminal',
-  wsSignalUrl: 'ws://localhost:8081/signal'
+  // apiUrl: 'http://localhost:8081',
+  // wsTerminalUrl: 'ws://localhost:8081/terminal',
+  // wsSignalUrl: 'ws://localhost:8081/signal'
+  apiUrl:'https://codezy-backend-185224543792.asia-south2.run.app',
+  wsSignalUrl:'wss://codezy-backend-185224543792.asia-south2.run.app/signal',
+  wsTerminalUrl:'wss://codezy-backend-185224543792.asia-south2.run.app/terminal'
 };
